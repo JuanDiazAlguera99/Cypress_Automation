effbd18cc6e5ff52947d56f2fe6fe556cf6ec17a
JuanDiazAlguera99 <71416234+JuanDiazAlguera99@users.noreply.github.com>
2022-08-16 15:32:12 -0600

    Qa (#1)
    * add gitignore
    
    * add gitignore

--- cypress/e2e/Login/login.cy.js
+++ cypress/e2e/Login/login.cy.js
@@ -1,8 +1,8 @@
-describe('login credentials', () => {
-  beforeEach("must be in log in page", () => {
-    cy.visit('/')
-  })
-  it('should not login with invalid credentials', () => {
-    cy.login('standard_user', 'secret_sauce')
-  })
-})
+describe("login credentials", () => {
+  beforeEach("must be in login page", () => {
+    cy.visit("/");
+  });
+  it("should not login with invalid credentials", () => {
+    cy.login("standard_user", "secret_sauce");
+  });
+});
