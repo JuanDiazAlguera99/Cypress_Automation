d91ddecda5bd4f3c735c645e2438786b22c7485a
JuanDiazAlguera99 <juancarlosdiazalguera@gmail.com>
2022-08-16 15:53:18 -0600

    add commads

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,23 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("validateText", (selector, text) => {
+  cy.get(selector).should("have.text", text);
+});
+
+Cypress.Commands.add("clickButton", (selector) => {
+  cy.get(selector).click();
+});
+
+Cypress.Commands.add("verifyUrl", (url) => {
+  cy.url().should("eq", url);
+});
+
+Cypress.Commands.add("shouldBeVisible", (selector) => {
+  cy.get(selector).should("be.visible");
+});
+
+Cypress.Commands.add("shouldNotExit", (selector) => {
+  cy.get(selector).should("not.exist");
+});
+
+Cypress.Commands.add("typeMessage", (selector, message) => {
+  cy.get(selector).clear().type(message);
+});
