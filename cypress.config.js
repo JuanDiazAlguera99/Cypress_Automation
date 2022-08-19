46f048d749ae56ebd5c79f3eb3e6957984550176
JuanDiazAlguera99 <71416234+JuanDiazAlguera99@users.noreply.github.com>
2022-08-19 10:54:00 -0600

    Mochawesome (#5)
    * reporst implementation
    
    * add gitignore
    
    * mochawesome implementation

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    video: false,
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+    reportDir: "cypress/report/mochawesome-report",
+  },
+
   e2e: {
     baseUrl: "https://www.saucedemo.com/",
     chromeWebSecurity: false,
