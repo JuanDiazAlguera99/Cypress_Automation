3279557c8970c57829597b7ac34b472ce6b97b5e
JuanDiazAlguera99 <71416234+JuanDiazAlguera99@users.noreply.github.com>
2022-09-20 14:21:10 -0600

    add new cypress test  (#21)
    * update readme.md
    
    * update config to not save videos
    
    * update config to not save videos
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update readme
    
    * update main.yml
    
    * add new test
    
    * add new test
    
    * custom commands
    
    * disable macOs in yml file
    
    * requirements
    
    * new cypress test

--- cypress/e2e/Login/login.cy.js
+++ cypress/e2e/Login/login.cy.js
@@ -2,7 +2,7 @@ describe("Test login credentials", () => {
   beforeEach("must be in login page", () => {
     cy.visit("/");
   });
-  it("should not login with invalid credentials and show promt of invalid user", () => {
+  it("should not login with invalid credentials and show the error-message-container", () => {
     cy.login("standard_user", "secret_");
     cy.errorLoginPromt();
   });
--- cypress/support/shoppingCart.js
+++ cypress/support/shoppingCart.js
@@ -1,45 +1,46 @@
-const add_to_cart_button = ".btn_inventory";
-const shopping_cart_button = ".shopping_cart_link";
-const chekout_button = '[data-test="checkout"]';
-const first_name_field = '[data-test="firstName"]';
-const last_name_field = '[data-test="lastName"]';
-const postal_code_field = '[data-test="postalCode"]';
-const continue_button = '[data-test="continue"]';
-const inventory_item_name = ".inventory_item_name";
-const finish_button = '[data-test="finish"]';
-const finish_tittle = ".title";
-const back_home_button = '[data-test="back-to-products"]';
-const shopping_cart_item = ".cart_item";
+const addToCartButton = ".btn_inventory";
+const shoppingCartButton = ".shopping_cart_link";
+const chekoutButton = '[data-test="checkout"]';
+const firstNameField = '[data-test="firstName"]';
+const lastNameField = '[data-test="lastName"]';
+const postalCodeField = '[data-test="postalCode"]';
+const continueButton = '[data-test="continue"]';
+const inventoryItemName = ".inventory_item_name";
+const finishButton = '[data-test="finish"]';
+const finishTittle = ".title";
+const backHomeButton = '[data-test="back-to-products"]';
+const shoppingCartItem = ".cart_item";
 
 Cypress.Commands.add("addProductToCart", (number) => {
-  cy.get(add_to_cart_button).eq(number).click();
+  cy.get(addToCartButton).eq(number).click();
   cy.scrollTo(1000, 0);
 });
 
-Cypress.Commands.add("addProductsToCart", (number) => {
-  for (let i = 0; i < number; i++) {
-    cy.get(add_to_cart_button).eq(i).click();
+
+Cypress.Commands.add("addProductsToCart", (numberOfProdcuts) => {
+  for (let i = 0; i < numberOfProdcuts; i++) {
+    cy.get(addToCartButton).eq(i).click();
     cy.scrollTo(1000, 0);
   }
 });
 
-Cypress.Commands.add("validatenumberOfProducts", (number) => {
-  cy.get(shopping_cart_item).eq(number);
+Cypress.Commands.add("validatenumberOfProducts", (numberOfProdcuts) => {
+  cy.get(shoppingCartItem).eq(numberOfProdcuts);
 });
 
 Cypress.Commands.add("goToShoppingCart", () => {
-  cy.clickButton(shopping_cart_button);
+  cy.clickButton(shoppingCartButton);
   cy.get('[data-test="remove-sauce-labs-backpack"]').should("be.visible");
 });
 
 Cypress.Commands.add("buyProduct", () => {
-  cy.clickButton(chekout_button);
-  cy.typeMessage(first_name_field, "juan");
-  cy.typeMessage(last_name_field, "diaz");
-  cy.typeMessage(postal_code_field, "1305");
-  cy.clickButton(continue_button);
-  cy.validateText(inventory_item_name, "Sauce Labs Backpack");
-  cy.clickButton(finish_button);
-  cy.validateText(finish_tittle, "Checkout: Complete!");
-  cy.clickButton(back_home_button);
+  cy.clickButton(chekoutButton);
+  cy.typeMessage(firstNameField, "juan");
+  cy.typeMessage(lastNameField, "diaz");
+  cy.typeMessage(postalCodeField, "1305");
+  cy.clickButton(continueButton);
+  cy.validateText(inventoryItemName, "Sauce Labs Backpack");
+  cy.clickButton(finishButton);
+  cy.validateText(finishTittle, "Checkout: Complete!");
+  cy.clickButton(backHomeButton);
 });
